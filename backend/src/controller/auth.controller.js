c6cc0256e7a34fc57a271621ce2b52278cab7e2d
Shuklax <harshshuklaknp@gmail.com>
2025-06-14 16:09:59 +0530

    implements the login auth flow

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,63 +1,106 @@
 import User from "../models/User.js";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
-export async function signup(req, res){
-    const {email, password, fullName} = req.body;
+export async function signup(req, res) {
+  const email = req.body.email?.trim();
+  const fullName = req.body.fullName?.trim();
+  const password = req.body.password;
 
-    try {
-        if(!email || !password || !fullName){
-            return res.status(400).json({message: "All fileds are required"});
-        }
-
-        if(password.length<8){
-            return res.status(400).json({message: "Password must be at least 8 characters"});
-        }
-
-        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-        if(!emailRegex.test(email)){
-            return res.status(400).json({message: "Invalid email format"});
-        }
-
-        const existingUser = await User.findOne({email});
-        if(existingUser){
-            return res.status(400).json({message: "Email already exists; enter a different one"});
-        }
-
-        //choose the random avatar functionality
-        const idx = Math.floor(Math.random() * 100) + 1; //generate random number b/w 1-100
-        const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`
-
-        const newUser = new User.create({
-            email, 
-            fullName,
-            password,
-            profilePic: randomAvatar,
-        })
-
-        const token = jwt.sign({userId:newUser._id}, process.env.JWT_SECRET_KEY, {
-            expresIn: "7d"
-        })
-
-        res.cookie("jwt", token, {
-            maxAge: 7*24*60*60*1000,
-            httpOnly: true, // prevents XSS attacks
-            sameSite: 'strict', //prevents CSRF attacks
-            secure: process.env.NODE_ENV==='production' //prevents HTTP requests
-        })
-
-        res.status(201).json({success:true, user:newUser});
-
-    } catch (error) {
-        
+  try {
+    if (!email || !password || !fullName) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-    
+
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 8 characters" });
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Email already exists; enter a different one" });
+    }
+
+    //choose the random avatar functionality
+    const idx = Math.floor(Math.random() * 100) + 1; //generate random number b/w 1-100
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    const newUser = await User.create({
+      email: email.toLowerCase(),
+      fullName,
+      password,
+      profilePic: randomAvatar,
+    });
+
+    const token = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, // prevents XSS attacks
+      sameSite: "strict", //prevents CSRF attacks
+      secure: process.env.NODE_ENV === "production", //prevents HTTP requests
+    });
+
+    res.status(201).json({ success: true, user: newUser });
+  } catch (error) {
+    console.log("Error in signup controller", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
-export async function login(req, res){
-    res.send('login router')
+export async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400).json({ message: "All fileds are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(401).json({ message: "Invalid email or password" });
+
+    const isPasswordCorrect = await user.matchPassword(password);
+    if (!isPasswordCorrect)
+      return res.status(401).json({ message: "Invalid email or password" });
+
+    const token = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, // prevents XSS attacks
+      sameSite: "strict", //prevents CSRF attacks
+      secure: process.env.NODE_ENV === "production", //prevents HTTP requests
+    });
+
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
-export function logout(req, res){
-    res.send('logout router')
+export function logout(req, res) {
+  res.send("logout router");
 }
