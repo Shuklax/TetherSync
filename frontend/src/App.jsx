f66f8f6faeb38f7425537df4ee8823b476f7cac6
Shuklax <harshshuklaknp@gmail.com>
2025-06-15 20:03:42 +0530

    implements react-router v7 and wraps all the pages in it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import NotificationsPage from "./pages/NotificationsPage";
+import OnBoardingPage from "./pages/OnBoardingPage";
+import CallPage from "./pages/CallPage";
+import ChatPage from "./pages/ChatPage";
 
 const App = () => {
   return (
     <div className="h-screen text-5xl" data-theme="night">
       <Routes>
-        
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/notifications" element={<NotificationsPage/>}/>
+        <Route path="/onboarding" element={<OnBoardingPage/>}/>
+        <Route path="/call" element={<CallPage/>}/>
+        <Route path="/chat" element={<ChatPage/>}/>
       </Routes>
     </div>
   );
