b3f065a9e6e6b1d37bcdf722487dfcff11453702
Shuklax <harshshuklaknp@gmail.com>
2025-06-16 13:24:48 +0530

    implements the signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
@@ -7,19 +7,34 @@ import NotificationsPage from "./pages/NotificationsPage";
 import OnBoardingPage from "./pages/OnBoardingPage";
 import CallPage from "./pages/CallPage";
 import ChatPage from "./pages/ChatPage";
+import toast, { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios";
 
 const App = () => {
+  const { data:authData, isLoading, error } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry: false, //multiple requests false
+  });
+  const authUser = authData?.user;
+
   return (
-    <div className="h-screen text-5xl" data-theme="night">
+    <div className="h-screen" data-theme="night">
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/signup" element={<SignUpPage/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/notifications" element={<NotificationsPage/>}/>
-        <Route path="/onboarding" element={<OnBoardingPage/>}/>
-        <Route path="/call" element={<CallPage/>}/>
-        <Route path="/chat" element={<ChatPage/>}/>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/notifications" element={authUser ? <NotificationsPage /> : <Navigate to="/" />} />
+        <Route path="/onboarding" element={authUser ? <OnBoardingPage /> : <Navigate to="/" />} />
+        <Route path="/call" element={authUser ? <CallPage /> : <Navigate to="/" />} />
+        <Route path="/chat" element={authUser ? <ChatPage /> : <Navigate to="/" />} />
       </Routes>
+
+      <Toaster />
     </div>
   );
 };
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,10 +1,193 @@
+import { useState } from "react";
+import { AudioWaveform } from "lucide-react";
+import { Link } from "react-router";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { axiosInstance } from "../lib/axios";
+import { signup } from "../lib/api";
 
 const SignUpPage = () => {
-  return (
-    <div>
-      SignUp Page
-    </div>
-  )
-}
+  const [signupData, setSignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
 
-export default SignUpPage
+  const queryClient = useQueryClient();
+
+  const { mutate: signupMutation, isPending, error } = useMutation({
+    mutationFn: signup,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+  });
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    signupMutation(signupData);
+  };
+
+  return (
+    <div
+      className="h-screen flex items-center justify-center p-4 sm:p-6 mid:p-8"
+      data-theme="forest"
+    >
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-10 rounded-xl shadow-lg overflow-hidden">
+        {/* Left side of sign up page */}
+        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+          {/* {LOGO} */}
+          <div className="mb-4 flex items-center justify-center gap-2">
+            <AudioWaveform className="size-9 text-primary" />
+            <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
+              TetherSync
+            </span>
+          </div>
+
+          {/* Error */}
+          {error && (
+            <div className="alert alert-error mb-4">
+              <span>{error.response.data.message}</span>
+            </div>
+          )}
+
+          <div className="w-full">
+            <form onSubmit={handleSignup}>
+              <div className="soace-y-4">
+                <div>
+                  <h2 className="text-xl font-semibold">Create an account</h2>
+                  <p className="text-sm opacity-70">
+                    Join TetherSync and start your language learning adventure!
+                  </p>
+                </div>
+
+                <div className="space-y-3">
+                  {/* FULLNAME */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Full Name</span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="JohnDoe"
+                      className="input input-bordered w-full"
+                      value={signupData.fullName}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...signupData,
+                          fullName: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                  </div>
+
+                  {/* EMAIL */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Email</span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="john@gmail.com"
+                      className="input input-bordered w-full"
+                      value={signupData.email}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...signupData,
+                          email: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                  </div>
+
+                  {/* PASSWORD  */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Password</span>
+                    </label>
+                    <input
+                      type="password"
+                      placeholder="**********"
+                      className="input input-bordered w-full"
+                      value={signupData.password}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...signupData,
+                          password: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <p className="text-xs opacity-70 mt-1">
+                      Password must be at least 8 characters long
+                    </p>
+                  </div>
+
+                  {/* agreement checkbox */}
+                  <div className="form-control">
+                    <label className="label cursor-pointer justify-start gap-2">
+                      <input
+                        type="checkbox"
+                        className="checkbox checkbox-sm"
+                        required
+                      />
+                      <span className="text-xs leading-tight">
+                        I agree to the{" "}
+                        <span className="text-primary hover:underline">
+                          terms of service
+                        </span>{" "}
+                        and{" "}
+                        <span className="text-primary hover:underline">
+                          privacy policy
+                        </span>
+                      </span>
+                    </label>
+                  </div>
+
+                  <button className="btn btn-primary w-full" type="submit">
+                    {isPending ? "Signing up....." : "Create Account"}
+                  </button>
+
+                  <div className="text-center mt-4">
+                    <p className="text-sm">
+                      Already have an account?{" "}
+                      <Link
+                        to="/login"
+                        className="text=primary hover:underline font-bold"
+                      >
+                        Sign In
+                      </Link>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        {/* right side of the signup page */}
+        <div className="hidden lg:flex w-full lg:w-1/2 bg-primary/10 items-center justify-center">
+          <div className="max-w p-8">
+            <div className="relative aspect-square mx-auto mx-w-sm">
+              <img
+                src="../../public/vd.png"
+                alt="illustration"
+                className="w-full h-full"
+              />
+            </div>
+
+            <div className="text-center space-y-3 mt-6">
+              <h2 className="text-xl font-semibold">
+                Connect with language partners worldwide
+              </h2>
+              <p className="opacity-70">
+                Practice conversation, make friends, and improve your language
+                skills together
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
